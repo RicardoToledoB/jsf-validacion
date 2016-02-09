fb574e03a26994e4b61358bbfa5d73e0b43d3716
ricardotoledo <ricardotoledo@192.168.1.39>
2016-02-09 17:16:24 -0300

    jsf reportes

--- src/main/java/com/proyecto/util/CheckRut.java
+++ src/main/java/com/proyecto/util/CheckRut.java
@@ -13,14 +13,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import javax.validation.ReportAsSingleViolation;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 
 @Documented
 @Constraint(validatedBy = ValidaRut.class)
 @Target(value = {ElementType.FIELD})
 @Retention(value = RetentionPolicy.RUNTIME)
+@NotNull
 public @interface CheckRut {
     public String message() default "Rut Invalido!";
 
